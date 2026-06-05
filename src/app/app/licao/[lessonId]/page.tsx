77e348cd5b03b2df8a3779e3e5b42c9424730858
munizlmachado-jpg <munizlmachado@gmail.com>
2026-06-05 12:12:23 -0300

    Randomize lesson exercises and options

--- src/app/app/licao/[lessonId]/page.tsx
+++ src/app/app/licao/[lessonId]/page.tsx
@@ -2,11 +2,21 @@ import { AppShell } from "@/components/AppShell";
 import { LessonWizard } from "@/components/exercises/LessonWizard";
 import { demoExercises, lessons } from "@/lib/data/catalog";
 import { listExercisesByLessonSlug } from "@/repositories/exercise-repository";
+import type { Exercise } from "@/types/exercise";
+import { shuffleItems } from "@/utils/shuffle";
 
 export default async function LessonPage({ params }: { params: Promise<{ lessonId: string }> }) {
   const { lessonId } = await params;
   const lesson = lessons.find((item) => item.id === lessonId) ?? lessons[0];
   const persistedExercises = await listExercisesByLessonSlug(lessonId);
   const exercises = persistedExercises.length ? persistedExercises : demoExercises.filter((exercise) => exercise.lesson_id === lessonId);
-  return <AppShell><LessonWizard lesson={lesson} exercises={exercises.length ? exercises : demoExercises.slice(0, 1)} /></AppShell>;
+  const shuffledExercises = shuffleExercises(exercises.length ? exercises : demoExercises.slice(0, 1));
+  return <AppShell><LessonWizard lesson={lesson} exercises={shuffledExercises} /></AppShell>;
+}
+
+function shuffleExercises(exercises: Exercise[]) {
+  return shuffleItems(exercises).map((exercise) => ({
+    ...exercise,
+    exercise_options: exercise.exercise_options ? shuffleItems(exercise.exercise_options) : exercise.exercise_options,
+  }));
 }
